cd47fa06661f8eb118eed211bec0bdae690f0100
Jacob Barta <jacob.barta25@gmail.com>
2023-02-06 14:56:29 -0500

    Use code I found online to do it for me

--- main.py
+++ main.py
@@ -2,6 +2,65 @@ import cv2 as cv
 import numpy as np
 import math
 
+def drawAxis(img, p_, q_, color, scale):
+    p = list(p_)
+    q = list(q_)
+
+    ## [visualization1]
+    angle = math.atan2(p[1] - q[1], p[0] - q[0])  # angle in radians
+    hypotenuse = math.sqrt((p[1] - q[1]) * (p[1] - q[1]) + (p[0] - q[0]) * (p[0] - q[0]))
+
+    # Here we lengthen the arrow by a factor of scale
+    q[0] = p[0] - scale * hypotenuse * math.cos(angle)
+    q[1] = p[1] - scale * hypotenuse * math.sin(angle)
+    cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv.LINE_AA)
+
+    # create the arrow hooks
+    p[0] = q[0] + 9 * math.cos(angle + math.pi / 4)
+    p[1] = q[1] + 9 * math.sin(angle + math.pi / 4)
+    cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv.LINE_AA)
+
+    p[0] = q[0] + 9 * math.cos(angle - math.pi / 4)
+    p[1] = q[1] + 9 * math.sin(angle - math.pi / 4)
+    cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv.LINE_AA)
+    ## [visualization1]
+def getOrientation(pts, img):
+    ## [pca]
+    # Construct a buffer used by the pca analysis
+    sz = len(pts)
+    data_pts = np.empty((sz, 2), dtype=np.float64)
+    for i in range(data_pts.shape[0]):
+        data_pts[i, 0] = pts[i, 0, 0]
+        data_pts[i, 1] = pts[i, 0, 1]
+
+    # Perform PCA analysis
+    mean = np.empty((0))
+    mean, eigenvectors, eigenvalues = cv.PCACompute2(data_pts, mean)
+
+    # Store the center of the object
+    cntr = (int(mean[0, 0]), int(mean[0, 1]))
+    ## [pca]
+
+    ## [visualization]
+    # Draw the principal components
+    cv.circle(img, cntr, 3, (255, 0, 255), 2)
+    p1 = (
+    cntr[0] + 0.02 * eigenvectors[0, 0] * eigenvalues[0, 0], cntr[1] + 0.02 * eigenvectors[0, 1] * eigenvalues[0, 0])
+    p2 = (
+    cntr[0] - 0.02 * eigenvectors[1, 0] * eigenvalues[1, 0], cntr[1] - 0.02 * eigenvectors[1, 1] * eigenvalues[1, 0])
+    drawAxis(img, cntr, p1, (255, 255, 0), 1)
+    drawAxis(img, cntr, p2, (0, 0, 255), 5)
+
+    angle = math.atan2(eigenvectors[0, 1], eigenvectors[0, 0])  # orientation in radians
+    ## [visualization]
+
+    # Label with the rotation angle
+    label = "  Rotation Angle: " + str(-int(np.rad2deg(angle)) - 90) + " degrees"
+    textbox = cv.rectangle(img, (cntr[0], cntr[1] - 25), (cntr[0] + 250, cntr[1] + 10), (255, 255, 255), -1)
+    cv.putText(img, label, (cntr[0], cntr[1]), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv.LINE_AA)
+
+    return angle
+
 cap = cv.VideoCapture("cone.mkv")
 
 if not cap.isOpened():
@@ -15,7 +74,7 @@ while cap.isOpened():
         imgHSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
         imgThresh = cv.inRange(imgHSV, (25, 100, 0), (35, 255, 255))
 
-        # cv.imshow("treshold", imgThresh)
+        cv.imshow("treshold", imgThresh)
 
         # Taking a matrix of size 5 as the kernel
         kernel = np.ones((5, 5), np.uint8)
@@ -26,34 +85,11 @@ while cap.isOpened():
 
         contours, hierarchy = cv.findContours(imgThresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
 
-
-        # imDisplay = imgThresh.copy()
         frameCopy = frame.copy()
 
         c = max(contours, key=cv.contourArea)
 
-        box = cv.minAreaRect(c)
-        rect = cv.boxPoints(box)
-        rect = np.int0(rect)
-
-        id = 0
-        for point in rect:
-            cv.putText(frameCopy, str(id), (point[0], point[1]), cv.FONT_HERSHEY_SIMPLEX, 4, (0,0,0), 2, cv.LINE_AA)
-            id += 1
-
-        
-
-        # print(box[0])
-
-        # originPoint = rect[0]
-        # endPoint = rect[3]
-        #
-        # point = [endPoint[0]-originPoint[0], endPoint[1]-originPoint[1]]
-        # angle = math.atan(point[0]/point[1]) * 180 / math.pi
-
-        cv.putText(frameCopy, str(box[2]), (150, 250), cv.FONT_HERSHEY_SIMPLEX, 4, (0,0,0), 2, cv.LINE_AA)
-        cv.drawContours(frameCopy, c, -1, (255, 0, 0), 3)
-        cv.drawContours(frameCopy, [rect], 0, (0, 0, 255), 3)
+        angle = getOrientation(c, frameCopy)
 
         cv.imshow('Contours', frameCopy)
 
@@ -67,7 +103,3 @@ while cap.isOpened():
 cap.release()
 
 cv.destroyAllWindows()
-
-
-def midpoint(point1, point2):
-    return [(point1[0] + point2[0])/2, (point1[1] + point2[1])/2]
